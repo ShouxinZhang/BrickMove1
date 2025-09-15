f7772de7d5df17ce71b2ed678ca328172012197e
张守信(Shouxin Zhang) <1571193735@qq.com>
2025-09-15 16:02:31 +0800

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,19 +1,8 @@
 import Lake
 open Lake DSL
 
-package «brickMove» where
-  version := v!"0.1.0"
-  leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
-    ⟨`pp.proofs.withType, false⟩,
-    ⟨`autoImplicit, false⟩,
-    ⟨`relaxedAutoImplicit, false⟩
-  ]
+package BrickMove1 where
+  -- Add any package-level configurations if needed
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.16.0"
-
-@[default_target]
-lean_lib «BrickMove» where
-  -- add library configuration options here
-  globs := #[.submodules `BrickMove]
